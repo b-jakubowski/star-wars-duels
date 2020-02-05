54e26b3d1866409cbdd09a679275cc6812f16e97
bartek <bjakubov@gmail.com>
2020-02-05 19:56:00 +0100

    feat: Add Homepage screen

---
+++ src/screens/Homepage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Homepage: React.FC = () => (
+	<Layout>
+		<p>Home page !</p>
+	</Layout>
+);
+
+export default Homepage;

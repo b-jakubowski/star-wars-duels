d87c157b000342c1553004a6762ee7d0b5525b12
bartek <bjakubov@gmail.com>
2020-02-05 20:00:25 +0100

    feat: Add routing to screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Homepage from './screens/Homepage';
+import Duel from './screens/Duel';
+import PageNotFound from './screens/PageNotFound';
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<Homepage />
+				</Route>
+				<Route path="/heroes">
+					<Duel unit={'heroes'} />
+				</Route>
+				<Route path="/starships">
+					<Duel unit={'starships'} />
+				</Route>
+				<Route path="*">
+					<PageNotFound />
+				</Route>
+			</Switch>
+		</Router>
 	);
 };
 

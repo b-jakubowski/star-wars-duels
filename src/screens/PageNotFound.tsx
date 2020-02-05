78069c2877fdda99f2489e4cda85ec4bbbca52c2
bartek <bjakubov@gmail.com>
2020-02-05 19:59:52 +0100

    feat: Add dummy PageNotFound screen

---
+++ src/screens/PageNotFound.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const PageNotFound: React.FC = () => (
+	<Layout>
+		<p>404 sorry, this page does not exist</p>
+	</Layout>
+);
+
+export default PageNotFound;

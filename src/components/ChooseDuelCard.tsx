497d8d9b5164d00b889f7a583da349485688d415
bartek <bjakubov@gmail.com>
2020-02-07 13:32:19 +0100

    feat: Add ChooseDuelCard component and implement routing when user clicks on it.

---
+++ src/components/ChooseDuelCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles, Card, CardContent, CardHeader, CardMedia } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+interface ChooseDuelCardProps {
+	title: string;
+	background: string;
+	route: string;
+}
+
+const useStyles = makeStyles({
+	link: {
+		textDecoration: 'none',
+	},
+	root: {
+		marginTop: 36,
+	},
+	media: {
+		height: 260,
+	},
+	title: {
+		textAlign: 'center',
+	},
+});
+
+const ChooseDuelCard: React.FC<ChooseDuelCardProps> = ({
+	title,
+	background,
+	route,
+}: ChooseDuelCardProps) => {
+	const classes = useStyles();
+
+	return (
+		<Link className={classes.link} to={route}>
+			<Card className={classes.root}>
+				<CardContent>
+					<CardHeader className={classes.title} title={title} />
+					<CardMedia className={classes.media} image={background} />
+				</CardContent>
+			</Card>
+		</Link>
+	);
+};
+
+export default ChooseDuelCard;
--- src/screens/Homepage.tsx
+++ src/screens/Homepage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import ChooseDuelCard from '../components/ChooseDuelCard';
+import duelPic from '../images/duel-pic.jpg';
+import starshipsPic from '../images/starships-pic.jpg';
 
 const Homepage: React.FC = () => (
 	<Layout>
-		<p>Home page !</p>
+		<ChooseDuelCard route="/heroes" title="Hero vs. Hero" background={duelPic} />
+		<ChooseDuelCard route="/starships" title="Starship vs. Starship" background={starshipsPic} />
 	</Layout>
 );
 

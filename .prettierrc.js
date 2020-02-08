51d98f49e8b6a9e476ed93e73afc93e762c212e0
bartek <bjakubov@gmail.com>
2020-02-08 11:22:28 +0100

    fix: Made bigger tolerance for printWidth in prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
     semi: true,
     trailingComma: 'all',
     singleQuote: true,
-    printWidth: 100,
+    printWidth: 120,
     useTabs: true,
     tabWidth: 2,
     noVarKeyword: true,

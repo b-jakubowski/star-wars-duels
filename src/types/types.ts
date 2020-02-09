aedcd12f0ea9413fc76c8e53d483c75612830c50
bartek <bjakubov@gmail.com>
2020-02-09 13:58:29 +0100

    feat: Add basic types

---
+++ src/types/types.ts
@@ -0,0 +1,25 @@
+export interface DuelProps {
+	type: ContestantType;
+}
+
+export enum ContestantType {
+	STARSHIPS = 'starships',
+	HEROES = 'heroes',
+}
+
+export enum DuelUnit {
+	CREW = 'crew',
+	MASS = 'mass',
+}
+
+export interface Contestant {
+	name: string;
+}
+
+export interface Hero extends Contestant {
+	[DuelUnit.MASS]: string;
+}
+
+export interface Starship extends Contestant {
+	[DuelUnit.CREW]: string;
+}

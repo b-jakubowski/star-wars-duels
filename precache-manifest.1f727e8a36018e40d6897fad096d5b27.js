51eb6b959628d79af94db72ead49b6c2574fcceb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-08 09:51:50 +0000

    Deploy b-jakubowski/star-wars-duels to github.com/b-jakubowski/star-wars-duels.git:gh-pages

---
+++ precache-manifest.1f727e8a36018e40d6897fad096d5b27.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00a6ae861098a1b0639d6d37363607f",
+    "url": "/star-wars-duels/index.html"
+  },
+  {
+    "revision": "705476676e286e4a8b21",
+    "url": "/star-wars-duels/static/js/2.8b6d4b72.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/star-wars-duels/static/js/2.8b6d4b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c864679d3ef87946370a",
+    "url": "/star-wars-duels/static/js/main.93d1e492.chunk.js"
+  },
+  {
+    "revision": "fc63d59134c4e5dc035b",
+    "url": "/star-wars-duels/static/js/runtime-main.3421fba1.js"
+  },
+  {
+    "revision": "266338eb9840fe9a4487fea2945b0075",
+    "url": "/star-wars-duels/static/media/duel-pic.266338eb.jpg"
+  },
+  {
+    "revision": "d2b278cf7558648923246f37fc297a6b",
+    "url": "/star-wars-duels/static/media/mandalorian.d2b278cf.png"
+  },
+  {
+    "revision": "3e98ee67a196b2845012d881ce744d3d",
+    "url": "/star-wars-duels/static/media/millennium-falcon-011.3e98ee67.jpg"
+  },
+  {
+    "revision": "488827718a94744d26676a1c5540f951",
+    "url": "/star-wars-duels/static/media/starship3.48882771.png"
+  },
+  {
+    "revision": "a02e14eb6f0a0600506446c6ece6c988",
+    "url": "/star-wars-duels/static/media/starships-pic.a02e14eb.jpg"
+  },
+  {
+    "revision": "adcf3351d0b748e7dad9d010aa726bfd",
+    "url": "/star-wars-duels/static/media/stormtrooper.adcf3351.png"
+  }
+]);
